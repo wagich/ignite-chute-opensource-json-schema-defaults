f273781bf2d76c5353b4a23f505c0ea7574316b9
Petr Bela <bela.petr@gmail.com>
2018-02-12 21:29:30 -0700

    changelog settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,6 +41,29 @@ module.exports = function(grunt) {
         gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
       }
     },
+
+    conventionalChangelog: {
+      options: {
+        changelogOpts: {
+          // conventional-changelog options go here
+        },
+        context: {
+          // context goes here
+        },
+        gitRawCommitsOpts: {
+          // git-raw-commits options go here
+        },
+        parserOpts: {
+          // conventional-commits-parser options go here
+        },
+        writerOpts: {
+          // conventional-changelog-writer options go here
+        }
+      },
+      release: {
+        src: 'CHANGELOG.md'
+      }
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
@@ -49,5 +72,6 @@ module.exports = function(grunt) {
 
   grunt.registerTask('default', ['build']);
   grunt.registerTask('build', ['jshint']);
+  grunt.registerTask('changelog', ['conventionalChangelog']);
 
 };
